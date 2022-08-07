0962b39fd04eec479d047f5d4c9d0ea4ba2cc7fc
pitamer <itamar@lightricks.com>
2022-08-07 18:01:35 +0300

    :goal_net: Add basic error handling for common cases

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::API
   include ActionController::MimeResponds
+
+  def handle_record_not_found(exception)
+    render json: { error: exception.message }.to_json, status: :not_found
+  end
+
+  def handle_invalid_transition(exception)
+    render json: { error: exception.message, traces: exception.backtrace }.to_json, status: :bad_request
+  end
+
+  def handle_internal_error(exception)
+    render json: { error: exception.message }.to_json, status: :internal_server_error
+  end
+
+  rescue_from ::ActiveRecord::RecordNotFound, with: :handle_record_not_found
+  rescue_from ::AASM::InvalidTransition, with: :handle_invalid_transition
+  rescue_from ::NameError, with: :handle_internal_error
 end
